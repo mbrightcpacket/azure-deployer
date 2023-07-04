2dd9a94620efc463e35031ba9870d0fb00fc7f9e
Martin Bright <mbright@cpacketnetworks.com>
2023-07-04 15:34:35 -0400

    fix: file permissions

--- create-cclearv-cloud-init.sh
+++ create-cclearv-cloud-init.sh
@@ -2,8 +2,11 @@
 set -e
 
 cloud_init_sh="cclearv-ccloud-init.sh"
+rm -f "$cloud_init_sh"
 touch "$cloud_init_sh"
+chmod 755 "$cloud_init_sh"
 
+# shellcheck disable=SC2129
 echo '#!/bin/bash' >> "$cloud_init_sh"
 echo 'mkdir -p /opt/cloud/' >> "$cloud_init_sh"
 echo 'cat <<EOF_DEPLOYER >/opt/cloud/deployer.py' >> "$cloud_init_sh"
