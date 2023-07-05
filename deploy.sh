ac78d83d5a57e96a727f17cb1dd27950369dd8bf
Martin Bright <mbright@cpacketnetworks.com>
2023-07-05 18:43:14 -0400

    fix: create rg

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ deployment="mbright-bicep"
 resource_group="mbright-bicep"
 template="main.bicep"
 parameters="parameters.json"
+location="eastus2"
+
+az group create --name "$resource_group" --location "$location"
+# az deployment group create --resource-group exampleRG --template-file main.bicep --parameters appInsightsLocation=<app-location>
 
 az deployment group create \
   --name "$deployment" \
